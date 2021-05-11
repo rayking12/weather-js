7e97ef50a7af855ab13e66868ab04d01f65b380d
Ray King <rayodo667@gmail.com>
2021-05-11 14:33:50 +0100

    Weather app initialize

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# weather-js
-A weather app built with vanilla JavaScript with openWeather Api
+# Weather App
+
+## About the app
+
+> In this project I built a weather app with javascript mastering async, callbacks, promises and api calls in javascript
+
+## Setup
+ To get started, you have to have get a api key from openWeathermap 
+> Navigate to https://openweathermap.org/api
+
+> Register with an account
+
+> Navigate to the Api tabs and request for a api
+
+> copy the Api key
+
+To get started, you should first get this file in your local machine by cloning or forking this project or typing in your terminal
+
+> git clone https://github.com/rayking12/weather-js.git
+
+> cd weather-js
+
+> Create a file name "apikey" in src folder and input your Api key
+
+```
+export const API_KEY = "{your_api_key}"
+}
+
+```
+> npm install
+
+> npx webpack --watch
+
+## Contributing
+
+Contributions, issues and feature requests are welcome! Start by:
+
+Forking the project
+
+Cloning the project to your local machine
+
+cd into the project directory
+
+> cd weather-js
+
+> Run git checkout -b your-branch-name
+
+Make your contributions
+
+Push your branch up to your forked repository
+
+Open a Pull Request with a detailed description to the development branch of the original project for a review
+
+---
+
+Built With
+
+This project was built using these technologies.
+
+```
+Javascript
+
+Webpack
+
+HTML/CSS
+
+
+ESlint
+
+VSCode editor
+
+Git
+
+Github
+```
+
+## Contributors
+
+## Ray King Odoh
+
+- GitHub [rayKing12](https://GitHub.com/rayking12)
+- Linkedin: [Ray King](https://www.linkedin.com/in/king-ray-514b89133/)
+
+## Contributions
+
+Contributions, issues and requests are welcome
+
+## Show support
+
+Give a star if you like the project
---
+++ src/index.js
@@ -0,0 +1,106 @@
+import anime from 'animejs/lib/anime.es';
+
+import {
+  weatherRequest,
+  fillResult,
+  filterData,
+  currentUnit,
+} from './data-handling';
+
+import { convertTemps } from './unit-convertion';
+
+let userCity = localStorage.getItem('user_city') || undefined;
+
+let mainScreen = document.querySelector('#main-screen');
+let resultScreen = document.querySelector('#result-screen');
+
+window.addEventListener('load', (e) => {
+  //Animate main screen
+  anime({
+    targets: mainScreen,
+    opacity: [0, 1],
+    translateY: [-10, 0],
+    duration: 3000,
+  });
+});
+
+let cityInput = document.querySelector('#search-input');
+let searchBtn = document.querySelector('#search-btn');
+searchBtn.addEventListener('click', submitHandler);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') submitHandler();
+});
+
+let errorMsg = document.querySelector('#error');
+
+async function submitHandler() {
+  if (validateInput()) {
+    try {
+      let fetchWeatherData = await weatherRequest(
+        cityInput.value
+      ).then((data) => fillResult(filterData(data)));
+
+      let resultScreenAnimation = anime({
+        targets: resultScreen,
+        opacity: [0, 1],
+        translateY: [-10, 0],
+        duration: 3000,
+        begin: () => {
+          mainScreen.style.display = 'none';
+          resultScreen.style.display = 'block';
+        },
+      });
+    } catch (e) {
+      console.log(e);
+      errorMsg.innerText = 'No location found!';
+      anime({
+        targets: errorMsg,
+        opacity: [0, 1],
+        begin: () => {
+          errorMsg.style.display = 'block';
+        },
+      });
+    }
+  } else {
+    let inputAnimation = await anime({
+      targets: cityInput,
+      translateX: [-20, 0],
+    });
+  }
+}
+
+function validateInput() {
+  return cityInput.value !== '';
+}
+
+//Return
+
+let returnBtn = document.querySelector('#return-btn');
+returnBtn.addEventListener('click', resetHandler);
+
+async function resetHandler() {
+  //Clean input
+  cityInput.value = '';
+  errorMsg.style.display = 'none';
+
+  let mainScreenAnimation = anime({
+    targets: mainScreen,
+    opacity: [0, 1],
+    translateY: [-10, 0],
+    duration: 3000,
+    begin: () => {
+      mainScreen.style.display = 'block';
+      resultScreen.style.display = 'none';
+    },
+  });
+}
+
+//Converting temps
+
+const convertBtn = document.querySelector('#convert-units-btn');
+convertBtn.addEventListener('click', (e) => {
+  convertTemps();
+  //Switch button text
+  convertBtn.innerText = convertBtn.innerText == 'F°' ? 'C°' : 'F°';
+});
